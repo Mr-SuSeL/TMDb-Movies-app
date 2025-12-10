aca5c7e75a210ee273b3e059494bdf14734bdff6
Łukasz Susłowicz <susel.invest@gmail.com>
2025-12-10 17:46:59 +0100

    Fix theme toggle - background switching works

---
+++ src/common/Layout/PageContainer.js
@@ -0,0 +1,9 @@
+// src/components/Layout/PageContainer.js
+import styled from "styled-components";
+
+export const PageContainer = styled.main`
+  max-width: 1368px;              /* szerokość jak na projekcie */
+  margin: 0 auto;                 /* wycentrowanie */
+  padding: 24px 16px 40px 16px;   /* odstępy od krawędzi */
+  box-sizing: border-box;
+`;
