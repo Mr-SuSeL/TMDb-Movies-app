7a3c1fae65e6901699a6fe7d0205cee8b6c9021b
Maciej Błaszczyk <maciejba64@gmail.com>
2025-12-27 17:47:13 +0100

    Creating a new branch and resolving conflicts

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,13 +1,6 @@
 // src/common/Navigation/index.js
 import React from "react";
-import {
-  Wrapper,
-  Container,
-  HeaderWrapper,
-  MenuWrapper,
-  SearchWrapper,
-  ThemeToggleWrapper,
-} from "./styled";
+import { Wrapper, Container } from "./styled";
 import Header from "./Header";
 import Menu from "./Menu";
 import Search from "./Search";
@@ -16,21 +9,10 @@ import { ThemeToggle } from "../ThemeToggle";
 const Navigation = ({ isDarkMode, onToggleTheme }) => (
   <Wrapper>
     <Container>
-      <HeaderWrapper>
-        <Header />
-      </HeaderWrapper>
-
-      <MenuWrapper>
-        <Menu />
-      </MenuWrapper>
-
-      <SearchWrapper>
-        <Search />
-      </SearchWrapper>
-
-      <ThemeToggleWrapper>
-        <ThemeToggle isDarkMode={isDarkMode} onToggle={onToggleTheme} />
-      </ThemeToggleWrapper>
+      <Header />
+      <Menu />
+      <Search />
+      <ThemeToggle isDarkMode={isDarkMode} onToggle={onToggleTheme} />
     </Container>
   </Wrapper>
 );
